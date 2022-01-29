a96a8008d50a88ae6ece74b7a9b2dffb1032a19a
rishabhjn321 <93904354+rishabhjn321@users.noreply.github.com>
2022-01-29 19:06:55 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// CSS
+import './index.css';
+
+import {data} from './books'
+import SpecificBook from './book'
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      {data.map((book, index) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
+      })}
+    </section>
+  );
+}
+
+
+
+ReactDom.render(<BookList />, document.getElementById('root'));
